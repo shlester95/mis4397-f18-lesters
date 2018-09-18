0b7d50acc7f57f6af4c0dd0123d2aa2049dd9f3d
shlester95 <shlester@uh.edu>
2018-09-18 13:50:39 -0500

    Removed all folders

--- ex7.rb
+++ ex7.rb
@@ -16,4 +16,4 @@ end10 = "g"
 end11 = "e"
 end12 = "r"
 
-print end1 + end2 + end3 + end4 + end5 + end6 + end7 +end8 + end9 +end10 + end11 + end12
+puts  end1 + end2 + end3 + end4 + end5 + end6 + end7 +end8 + end9 +end10 + end11 + end12
