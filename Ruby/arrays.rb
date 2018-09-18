95e23562f7d58abc3f8f6455abeeca70f00a2f44
shlester95 <shlester@uh.edu>
2018-09-18 07:46:40 -0500

    Moved all Ruby files to new Ruby folder and added more files

---
+++ Ruby/arrays.rb
@@ -0,0 +1,32 @@
+
+fruits = ['apple', 'orange', 'banana']
+# puts fruits
+# puts fruits.class # remember this method...
+
+cars = ['Ford', 'Honda', ['Toyota','1999']]
+puts cars
+puts cars.class
+
+p cars.first
+p cars.last
+p cars.last.first
+
+p cars[1]
+p cars[2]
+p cars.length
+p cars.size
+
+students = [student1, student2, student3,]
+# student object contains the following attributes
+# f_name
+# l_name
+# pid
+# car_model
+# dob
+
+# sort these students by last name
+
+# Iterate through the students array
+# for every studnet in the students array
+# grab a student, and read the last name
+# 
